69baad6d57ddc5b4cc1c205388b91328596cf58e
MatthiasVandeCasteele <71661057+Matthias-VdC@users.noreply.github.com>
2021-09-28 20:32:59 +0200

    Week 1: Exercise 6

--- js/index.js
+++ js/index.js
@@ -1,15 +1,52 @@
 "use strict";
 
-
 window.onload = () => {
+    let order = {}
     let form = document.getElementById("form").addEventListener("submit", e => {
         e.preventDefault();
         let name = document.getElementById("name").value;
         let email = document.getElementById("email").value;
-        let order = document.getElementById("order").value;
+        let orderInput = document.getElementById("order").value;
+
+        order = {
+            "name": name,
+            "email": email,
+            "order": orderInput
+        };
+
+        function printOrder(orderdetails) {
+            return `The order for the customer <b>${order.name}</b>  is the following:  <b>${order.order}</b>. The customer may be notified by email:  <b>${order.email}</b>`
+        }
+
         let text = document.getElementById("text").innerHTML =
-            orderText(name, email, order);
+            printOrder();
     });
+
+    let dishes = [];
+    dishes.push({
+        id: '1',
+        name: 'Burger and french fries',
+        price: '18'
+    }, {
+        id: '2',
+        name: 'Caviar',
+        price: '99'
+    }, {
+        id: '3',
+        name: 'Clam soup',
+        price: '14'
+    });
+
+    console.log(dishes);
+
+    for (let dish of dishes) {
+        console.log(dish);
+        document.getElementById("order").insertAdjacentHTML("afterend",
+            `
+        <input type="radio" name="choice" value="${dish.name}" id="${dish.id}"></input>
+        <label for="${dish.id}">${dish.name}</label>
+        `)
+    }
 }
 
 function orderText(name, email, order) {
