2cfb55f98f89c3f65e46e2ce5486474f6dc80964
MatthiasVandeCasteele <71661057+Matthias-VdC@users.noreply.github.com>
2021-10-19 15:35:47 +0200

    fix fetch function
    removed callback

--- Technical test/js/index.js
+++ Technical test/js/index.js
@@ -51,15 +51,12 @@ let data;
 let randomNumber = Math.floor(Math.random() * 59);
 
 window.onload = function () {
-    async function getData(url, _callback) {
+    async function getData(url) {
         let response = await fetch(url);
-        data = await response.json();
-        _callback();
-        return data;
+        return await response.json();
     }
 
     getData(`https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=${randomNumber}`, async () => {
-        await data;
         console.log(data);
         document.getElementById("randomThumb").innerHTML = `<img src="${data[randomNumber].thumb}" alt="${data[randomNumber].title}">`;
         document.getElementById("randomTitle").innerHTML = `<p>Random game: <b>${data[randomNumber].title}</p>`;
