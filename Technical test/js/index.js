0c7cc44712117f17f24621ffb30cdd930e28b365
MatthiasVandeCasteele <71661057+Matthias-VdC@users.noreply.github.com>
2021-10-19 14:05:04 +0200

    comment threejs

--- Technical test/js/index.js
+++ Technical test/js/index.js
@@ -5,43 +5,43 @@
 //THREE.JS CODE//
 /////////////////
 
-import * as THREE from "./three/build/three.module.js";
+// import * as THREE from './three/src/Three.js';
 
 
-let scene = new THREE.Scene();
-let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-let renderer = new THREE.WebGLRenderer({
-    antialias: true,
-    canvas: document.getElementById("canvas")
-});
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setClearColor(new THREE.Color('black'));
-document.body.appendChild(renderer.domElement);
+// let scene = new THREE.Scene();
+// let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+// let renderer = new THREE.WebGLRenderer({
+//     antialias: true,
+//     canvas: document.getElementById("canvas")
+// });
+// renderer.setSize(window.innerWidth, window.innerHeight);
+// renderer.setClearColor(new THREE.Color('black'));
+// document.body.appendChild(renderer.domElement);
 
-//add testcube
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshPhongMaterial({
-    color: "red",
-});
-const cube = new THREE.Mesh(geometry, material);
+// //add testcube
+// const geometry = new THREE.BoxGeometry(1, 1, 1);
+// const material = new THREE.MeshPhongMaterial({
+//     color: "red",
+// });
+// const cube = new THREE.Mesh(geometry, material);
 
-scene.add(cube);
+// scene.add(cube);
 
-let light = new THREE.HemisphereLight(0xf6e86d, 0x404040, 0.5);
-scene.add(light);
+// let light = new THREE.HemisphereLight(0xf6e86d, 0x404040, 0.5);
+// scene.add(light);
 
 
-camera.position.z = 10;
+// camera.position.z = 10;
 
-let animate = function () {
-    requestAnimationFrame(animate);
-    cube.position.set(0, 8 - window.scrollY / 520, 0);
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-    renderer.render(scene, camera);
-};
+// let animate = function () {
+//     requestAnimationFrame(animate);
+//     cube.position.set(0, 8 - window.scrollY / 520, 0);
+//     cube.rotation.x += 0.01;
+//     cube.rotation.y += 0.01;
+//     renderer.render(scene, camera);
+// };
 
-animate();
+// animate();
 
 /////////////////////
 //NON-THREE.JS code//
